a34d520d1897921b3d6c45f57272ceea28db750e
Frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-05-02 10:15:42 -0500

    se agrego microservice de api gateway

---
+++ ApiGateway/src/test/java/com/frankly221/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankly221.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
