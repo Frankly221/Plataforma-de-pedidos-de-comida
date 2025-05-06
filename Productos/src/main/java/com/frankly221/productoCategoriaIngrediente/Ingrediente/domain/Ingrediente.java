07f0a70e8c08d8571db72dcac6d1c47197bcb986
frankly221 <alexis.delcastillo@upeu.edu.pe>
2025-05-05 22:17:52 -0500

    se agrego entity Ingredientes

---
+++ Productos/src/main/java/com/frankly221/productoCategoriaIngrediente/Ingrediente/domain/Ingrediente.java
@@ -0,0 +1,42 @@
+package com.frankly221.productoCategoriaIngrediente.Ingrediente.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Ingrediente {
+
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ingrediente")
+    private int idIngrediente;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "creado_en")
+    private Instant creadoEn;
+    
+    @Column(name = "actualizado_en")
+    private Instant actualizadoEn;
+
+    
+}
